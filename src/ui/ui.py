48de74e8e6bfceb556c549e1c27c92c9535b1eef
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-04-12 14:54:13 +0300

    feat[ui]: dynamic windows and api key check

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,25 +1,28 @@
 from ui.weather_view import WeatherView
 from ui.login_view import LoginView
+from services import ConfigService
 
 
 class UI:
     def __init__(self, root):
         self._root = root
         self._current_view = None
-          # set window size
-        window_width = 400
-        window_height = 200
+        self._config = ConfigService()
+        # set window size
         # get the screen dimension
         screen_width = self._root.winfo_screenwidth()
         screen_height = self._root.winfo_screenheight()
         # find the center point
-        center_x = int(screen_width/2 - window_width / 2)
-        center_y = int(screen_height/2 - window_height / 2)
+        center_x = int(screen_width/2)
+        center_y = int(screen_height/2)
         # set the position of the window to the center of the screen
-        self._root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+        self._root.geometry("+%d+%d" % (center_x, center_y))
 
     def start(self):
-        self._show_login_view()
+        if self._config.api_key_is_set():
+            self._handle_weather()
+        else:
+            self._handle_login()
 
     def _hide_current_view(self):
         if self._current_view:
@@ -29,9 +32,13 @@ class UI:
     def _handle_weather(self):
         self._show_weather_view()
 
+    def _handle_login(self):
+        self._show_login_view()
+
     def _show_login_view(self):
         self._hide_current_view()
-        self._current_view = LoginView(self._root, self._handle_weather)
+        self._current_view = LoginView(
+            self._root, self._handle_weather, self._config)
         self._current_view.pack()
 
     def _show_weather_view(self):

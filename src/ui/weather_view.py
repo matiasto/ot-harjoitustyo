dcaea095d71ca551e6b449b99b4a08b79bc99272
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-04-12 14:58:04 +0300

    feat[UI]: added forecast and weather icons

--- src/ui/weather_view.py
+++ src/ui/weather_view.py
@@ -1,4 +1,9 @@
+from cgitb import text
+from io import BytesIO
+from time import time
 from tkinter import ttk, constants, StringVar
+from PIL import Image, ImageTk
+import requests
 from services import WeatherService
 
 
@@ -9,6 +14,7 @@ class WeatherView:
         self._frame = None
         self._city = None
         self._options = {'padx': 5, 'pady': 5}
+        self._icons = []
         self._initialize()
 
     def pack(self):
@@ -16,45 +22,78 @@ class WeatherView:
 
     def destroy(self):
         self._frame.destroy()
-    
-    def get_weather(self):
-        data = self._weather.current_weather(self._city.get())
 
-        self.result_label_city.config(text=data.city_name)
-        temp_result = f"Lämpötila: {data.temperature} C"
-        self.result_label_temp.config(text=temp_result)
-        humidity_result = f"Ilmankosteus: {data.humidity} %"
-        self.result_label_humidity.config(text=humidity_result)
-        pressure_result = f"Ilmanpaine: {data.pressure} hPa"
-        self.result_label_pressure.config(text=pressure_result)
-        report_result = f"Tiedote: {data.report}"
-        self.result_label_report.config(text=report_result)
+    def _geticon(self, iconcode):
+        url = "http://openweathermap.org/img/wn/" + iconcode + "@2x.png"
+        response_img = requests.get(url)
+        raw_data = response_img.content
+        img_data = Image.open(BytesIO(raw_data))
+        img = ImageTk.PhotoImage(img_data)
+        return img
 
-        
+    def _set_header(self, city_name):
+        header = ttk.Label(
+            self._frame, text=f"Weather in {city_name}", font=("Arial", 25))
+        header.grid(row=0, column=3, columnspan=5, sticky=(constants.W))
+
+    def _current(self, current):
+        self._icons.append(self._geticon(current.icon))
+        label_frame = ttk.Labelframe(self._frame)
+        time = ttk.Label(label_frame, text=f"Today at {current.time}")
+        icon = ttk.Label(label_frame, image=self._icons[0])
+        temp = ttk.Label(label_frame, text=f"{current.temperature} C")
+        report = ttk.Label(label_frame, text=f"{current.report}")
+
+        time.grid(row=1, column=0)
+        icon.grid(row=2, column=0)
+        temp.grid(row=3, column=0)
+        report.grid(row=4, column=0)
+        label_frame.grid(row=1, column=0, rowspan=4, **self._options)
+
+    def _forecast(self, forecast):
+        for i, day in enumerate(forecast, start=1):
+            self._icons.append(self._geticon(day.icon))
+            label_frame = ttk.Labelframe(self._frame)
+            time = ttk.Label(label_frame, text=day.time)
+            image = ttk.Label(label_frame, image=self._icons[i])
+            temp = ttk.Label(label_frame, text=f"{day.temperature} C")
+            report = ttk.Label(label_frame, text=day.report)
+
+            time.grid(row=1, column=0)
+            image.grid(row=2, column=0)
+            temp.grid(row=3, column=0)
+            report.grid(row=4, column=0)
+
+            label_frame.grid(row=1, column=i, rowspan=4, **self._options)
+
+    def _get_weather(self):
+        city = self._city.get()
+        data = self._weather.weather(city)
+        for item in self._frame.winfo_children():
+            item.destroy()
+        self._navbar(city)
+        self._frame.grid_forget()
+        self._set_header(city)
+        self._current(data.current)
+        self._forecast(data.forecast)
+
+    def _navbar(self, city):
+        self._city = StringVar(value=city)
+        label_frame = ttk.Labelframe(self._frame)
+
+        city_entry = ttk.Entry(
+            label_frame, textvariable=self._city, takefocus=True)
+        get_weather_button = ttk.Button(
+            label_frame,
+            text='Search',
+            command=self._get_weather
+        )
+
+        city_entry.grid(column=0, row=0, sticky=constants.W, **self._options)
+        get_weather_button.grid(column=1, row=0, sticky=constants.W)
+        label_frame.grid(row=0, column=0, columnspan=5, sticky=(constants.W))
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        self._city = StringVar()
-        self.city_label = ttk.Label(master=self._frame, text="City")
-        self.city_label.grid(column=0, row=0, sticky=constants.W, **self._options)
-        
-        self.city_entry = ttk.Entry(self._frame, textvariable=self._city)
-
-        self.result_label_city = ttk.Label(self._frame)
-        self.result_label_temp = ttk.Label(self._frame)
-        self.result_label_humidity = ttk.Label(self._frame)
-        self.result_label_pressure = ttk.Label(self._frame)
-        self.result_label_report = ttk.Label(self._frame)
-
-        self.get_weather_button = ttk.Button(
-            self._frame, 
-            text='Current Weather',
-            command=self.get_weather
-        )
-        self.city_entry.grid(column=1, row=0, sticky=constants.W, **self._options)
-        self.get_weather_button.grid(column=2, row=0, sticky=constants.W)
-        self.result_label_city.grid(row=1, columnspan=3, **self._options)
-        self.result_label_temp.grid(row=2, columnspan=3, **self._options)
-        self.result_label_humidity.grid(row=3, columnspan=3, **self._options)
-        self.result_label_pressure.grid(row=4, columnspan=3, **self._options)
-        self.result_label_report.grid(row=5, columnspan=3, **self._options)
-    
+        self._navbar("Helsinki")
+        self._get_weather()

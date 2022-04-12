de6f935a0c71ee9a95fe20aff55932b93d59fc98
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-04-12 15:00:20 +0300

    refactor: uniformity

--- tasks.py
+++ tasks.py
@@ -5,13 +5,16 @@ from invoke import task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
+
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
+
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest", pty=True)
+
 
 @task(coverage)
 def coverage_report(ctx):

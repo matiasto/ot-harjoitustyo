92853c4a46adf8d78dda5d83c7b7d083e69ee011
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-04-26 16:42:23 +0300

    feat: historical data for graphs

--- src/services/weather_service.py
+++ src/services/weather_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import requests
 from entities import Weather
 from .config_service import ConfigService
@@ -7,6 +8,7 @@ class WeatherService:
     def __init__(self) -> None:
         self.__config = ConfigService()
 
+    # Handles all the requests
     def __request(self, url: str) -> dict:
         data_request = requests.get(url)
         data = data_request.json()
@@ -22,13 +24,27 @@ class WeatherService:
         data = self.__request(url)[0]
         return (str(data["lat"]), str(data["lon"]))
 
+    # Onecall API retrieves current and 7 day forecast weather
     def __weather_data(self, latitude: str, longitude: str) -> dict:
-        url = self.__config.open_weather_url + "lat=" + latitude + "&lon=" + longitude + \
-            "&exclude=minutely,hourly,alerts" + "&appid=" + \
+        url = self.__config.open_weather_url + "?" + "lat=" + latitude + "&lon=" + longitude + \
+            "&exclude=minutely,alerts" + "&appid=" + \
             self.__config.api_key + "&units=metric"
         return self.__request(url)
 
-    def weather(self, city: str) -> dict:
+    # Regarding historical data, the api call has to be called separately on past 5 days
+    def __historical_weather_data(self, latitude: str, longitude: str) -> dict:
+        historical_data = []
+        today = datetime.now()
+        for i in range(1, 6):
+            day = str(int(datetime.timestamp(today - timedelta(days=i))))
+            url = self.__config.open_weather_url + "/timemachine?" + "lat=" + latitude + \
+                "&lon=" + longitude + "&dt=" + day + "&appid=" + \
+                self.__config.api_key + "&units=metric"
+            historical_data = historical_data + self.__request(url)["hourly"]
+        return historical_data
+
+    def weather(self, city: str) -> object:
         latitude, longitude = self.__location(city)
         weather_data = self.__weather_data(latitude, longitude)
-        return Weather(city, weather_data)
+        historical_data = self.__historical_weather_data(latitude, longitude)
+        return Weather(city, weather_data, historical_data)

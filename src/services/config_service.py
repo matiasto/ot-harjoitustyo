f9a2e5bb0f029d806aa258d34662b7f9b65a6baf
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-04-29 17:15:26 +0300

    docs[services]: added docstring

--- src/services/config_service.py
+++ src/services/config_service.py
@@ -3,8 +3,22 @@ import os
 
 
 class ConfigService:
+    """Administers API configurations.
+
+    Attributes:
+        arser: Instanse of ConfigParser that reads and writes config.ini file.
+        config_file_path: File path for configuration file.
+        open_weather_url: The base url for Onecall API used to retrieve weather data.
+        geocoding_url: The base url for Geocoding API used to convert city name to latitude and longitude.
+        api_key: The key required to access the OpenWeather- and Geocoding API.
+    """
 
     def __init__(self) -> None:
+        """The class constuctor.
+
+        Initializes the class by reading the existing config.ini file
+        and setting the attributes.
+        """
         self.__parser = ConfigParser()
         self.__config_file_path = os.path.join(
             os.path.dirname(__file__), "..", "config.ini")
@@ -16,28 +30,50 @@ class ConfigService:
 
     @property
     def open_weather_url(self):
+        """the Open Weather - One Call API base url."""
+
         return self.__open_weather_url
 
     @property
     def geocoding_url(self):
+        """the Open Weather - Geocoding API base url."""
+
         return self.__geocoding_url
 
     @property
     def api_key(self):
+        """the Open Weather - API Key."""
         return self.__api_key
 
     @api_key.setter
     def api_key(self, new_key: str):
+        """Sets API key.
+
+        Saves the key to config.ini.
+
+        Args:
+            new_key (str): New key from the user.
+        """
+
         self.__parser.set("USER", "api_key", new_key)
         with open(self.__config_file_path, 'w', encoding="utf8") as configfile:
             self.__parser.write(configfile)
 
     def api_key_is_set(self) -> bool:
+        """Check if API key is set.
+
+        Returns:
+            bool: Is set?
+        """
         if self.api_key != "":
             return True
         return False
 
     def __initialize(self):
+        """Initialize atributes.
+
+        Reads config.ini file and sets corresponding atribute values.
+        """
         self.__parser.read(self.__config_file_path)
         self.__open_weather_url = self.__parser["DEFAULT"]["open_weather_url"]
         self.__geocoding_url = self.__parser["DEFAULT"]["geocoding_url"]
--- src/services/weather_service.py
+++ src/services/weather_service.py
@@ -5,45 +5,118 @@ from .config_service import ConfigService
 
 
 class WeatherService:
+    """Fetches weather data from the API.
+
+    The weather() method works as the class endpoint
+    and is therefore the only public method.
+
+    Given the city name as the input, the name is first converted to
+    longitude and latitude by location() method. Then, that location
+    data is used to retrieve historical, current and forecast weather data.
+    The data is then remodeled to Weather object and returned.
+
+    Attributes:
+        config: object instance that includes API base urls and api key.
+    """
+
     def __init__(self) -> None:
+        """Class constructor.
+        
+        Creates an instance of config service.
+        """
+
         self.__config = ConfigService()
 
-    # Handles all the requests
     def __request(self, url: str) -> dict:
+        """Handles all requests.
+
+        Makes the request to appointed url, parses data
+        from json respond, and returns the data as dictionary.
+
+        Args:
+            url (str): The request url.
+
+        Raises:
+            Exception: The request failed.
+
+        Returns:
+            dict: The data from succesful request.
+        """
         data_request = requests.get(url)
         data = data_request.json()
         if data_request.status_code == 200 and data != []:
-            # getting data in the json format
             return data
-        raise Exception
+        raise Exception("the request failed")
 
-    # Using Geocoding API, converts city name to longitude and latitude
     def __location(self, city: str) -> tuple:
+        """Using Geocoding API, converts city name to longitude and latitude.
+
+        Converts user input city name to longitude and latitude.
+
+        Args:
+            city (str): Cities name from user input.
+
+        Returns:
+            tuple: Longitude and latitude are float values in string format.
+        """
         url = self.__config.geocoding_url + "q=" + \
             city + "&appid=" + self.__config.api_key
+        url = f"{self.__config.geocoding_url}q={city}&appid={self.__config.api_key}"
         data = self.__request(url)[0]
         return (str(data["lat"]), str(data["lon"]))
 
-    # Onecall API retrieves current and 7 day forecast weather
     def __weather_data(self, latitude: str, longitude: str) -> dict:
-        url = self.__config.open_weather_url + "?" + "lat=" + latitude + "&lon=" + longitude + \
-            "&exclude=minutely,alerts" + "&appid=" + \
-            self.__config.api_key + "&units=metric"
+        """Using Onecall API retrieves the current and 7 day forecast weather.
+
+        Args:
+            latitude (str): Input city latitude.
+            longitude (str): Input city longitude.
+
+        Returns:
+            dict: The weather data in dict format.
+            keys of interest include "current", "hourly", and "daily".
+            Keys "hourly" and "daily" include a list of dictionaries
+            describing their respective weather conditions.
+        """
+        url = f"{self.__config.open_weather_url}?lat={latitude}&lon={longitude}&exclude=minutely,alerts&appid={self.__config.api_key}&units=metric"
         return self.__request(url)
 
-    # Regarding historical data, the api call has to be called separately on past 5 days
-    def __historical_weather_data(self, latitude: str, longitude: str) -> dict:
+    def __historical_weather_data(self, latitude: str, longitude: str) -> list:
+        """Using Onecall API retrieves the 5 day historical weather.
+
+        The API requires separate requests for each day.
+
+        Args:
+            latitude (str): Input city latitude.
+            longitude (str): Input city longitude.
+
+        Returns:
+            list: List of hourly historical data.
+            Each hours weather is represented as dictionary.
+        """
         historical_data = []
         today = datetime.now()
         for i in range(1, 6):
             day = str(int(datetime.timestamp(today - timedelta(days=i))))
-            url = self.__config.open_weather_url + "/timemachine?" + "lat=" + latitude + \
-                "&lon=" + longitude + "&dt=" + day + "&appid=" + \
-                self.__config.api_key + "&units=metric"
+            url = f"{self.__config.open_weather_url}/timemachine?lat={latitude}&lon={longitude}&dt={day}&appid={self.__config.api_key}&units=metric"
             historical_data = historical_data + self.__request(url)["hourly"]
         return historical_data
 
     def weather(self, city: str) -> object:
+        """The class endpoint.
+        
+        Given the city input, the method calls for location() on the input.
+        The returned latitude and longitude are then fed to
+        weather_data() and historical_data().
+        The retrieved data is remodeled to Weather object
+        and returned.
+
+        Args:
+            city (str): User input city name.
+
+        Returns:
+            object: Weather object.
+        """
         latitude, longitude = self.__location(city)
         weather_data = self.__weather_data(latitude, longitude)
         historical_data = self.__historical_weather_data(latitude, longitude)

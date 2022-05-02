da72b14441e9b25340fa6c7095a4871deca13773
Matias Tolppanen <matias.tolppanen@helsinki.fi>
2022-05-02 22:45:26 +0300

    docs: docstring

--- src/services/icon_service.py
+++ src/services/icon_service.py
@@ -5,10 +5,24 @@ from .config_service import ConfigService
 
 
 class IconService:
+    """Serves the icons that describe the weather."""
+
     def __init__(self) -> None:
         self.__base_url = ConfigService().icon_url
 
     def geticon(self, iconcode: str) -> object:
+        """Retrieves icon by id
+
+        Uses icon id retrieved from WeatherService API call 
+        to request an image from OpenWeather API endpoint.
+
+        Args:
+            iconcode (str): id("10d")
+
+        Returns:
+            object: tk compatible image.
+        """
+        
         url = f"{self.__base_url}{iconcode}@2x.png"
         response_img = requests.get(url)
         raw_data = response_img.content
